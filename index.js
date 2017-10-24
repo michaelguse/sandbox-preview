8c34bd6c9aabde32b7762787831f72bef432a97d
Michael Guse <mguse@salesforce.com>
2017-10-24 19:26:44 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -17,6 +16,8 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+var pg = require('pg');
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
