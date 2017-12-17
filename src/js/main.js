6a639951f30d8746ae790848cbb492824023af99
Michelle Lamoureaux <mlamoureaux@salesforce.com>
2017-12-16 21:41:42 -0800

    added link to video, nav link, fixed header size on mobile

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,8 @@ $('.upgrade-option').click(function(){
   var link = $(this);
   link.next('.upgrade-info').toggleClass('reveal');
 });
+
+var currentPage = $(location).attr('pathname');
+if(currentPage.indexOf('upgrade-info') > -1){
+  $('.back-link').addClass('reveal');
+}
