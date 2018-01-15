5374152922735c0f7183a9814a9dcaf3647f8045
Michael Guse <guse.michael@gmail.com>
2018-01-15 10:10:35 -0600

    Add form validation functionality (#24)
    * Added form validation to index page
    
    * Added body-parser package
    
    * Added autofocus, error message and field validation
    
    * Added validation for correctly formed but invalid org_ids

--- index.js
+++ index.js
@@ -23,10 +23,14 @@ var config = {
 var pool = new Pool(config);
 var qryres = "";
 
-var express = require('express');
-var upperCase = require('upper-case');
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  form = require('express-form'),
+  filter = form.filter,
+  validate = form.validate;
 
 var app = express();
+app.use(bodyParser());
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -36,48 +40,66 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  pool.query('SELECT internal_rel_name, external_rel_name, org_id, org_type FROM rel_org_type', function(err, result) {
-    if (err) {
-      console.error(err);
-      response.send('Error: ' + err);
-    } else {
-//        response.render('pages/index', {
-        response.render('pages/index', {
+// Home page request
+app.get('/',
+  function (request, response) {
+    pool.query('SELECT internal_rel_name, external_rel_name, org_id, org_type FROM rel_org_type', function (err, result) {
+      if (err) {
+        console.error(err);
+        response.send('Error: ' + err);
+      } else {
+        response.render('pages/index', { results: result.rows  });
+      }
+    });
+  }
+);
+
+//Sandbox upgrade page request
+app.get('/upgrade',
+    // Form filter and validation for upgrade page 
+    form(
+      filter("org_id").trim().toUpper(),
+      validate("org_id").required().is(/^([csCS]{2}[1]?[0-9]{1,2})$/,"Invalid %s - try again!")
+    ),
+    function (request, response) {
+      if (!request.form.isValid) {
+        // Handle errors
+        console.log(request.form.errors);
+        response.render('pages/index.ejs', { errors: request.form.errors });
+      } else {
+        pool.query('SELECT id, internal_rel_name, external_rel_name, org_id, org_type FROM rel_org_type WHERE org_id = $1', [request.form.org_id], function (err, result) {
+          if (err) {
+            console.error(err);
+            response.send('Error: ' + err);
+          } else {
+            qryres = result.rows;
+            if (qryres.length > 0) {
+              console.log(qryres);
+              response.render('pages/upgrade', {
+                results: qryres,
+              });
+            } else {
+              console.log("[ 'Invalid entry - try again!' ]");
+              response.render('pages/index.ejs', { errors: [ 'Invalid entry - try again!' ] });
+            }
+          }
+        });
+      }
+    });
+
+    app.get('/db', function (request, response) {
+      pool.query('SELECT * FROM rel_org_type', function (err, result) {
+        if (err) {
+          console.error(err);
+          response.send('Error ' + err);
+        } else {
+          response.render('pages/db', {
             results: result.rows,
+          });
+        }
       });
-    }
-  });
-});
+    });
 
-app.get('/upgrade', function(request, response) {
-  pool.query('SELECT id, internal_rel_name, external_rel_name, org_id, org_type FROM rel_org_type WHERE org_id = $1', [upperCase(request.query.org_id)], function(err, result) {
-    if (err) {
-      console.error(err);
-      response.send('Error: ' + err);
-    } else {
-      qryres = result.rows;
-      console.log(qryres);
-      response.render('pages/upgrade', {
-        results: qryres,
-      });
-    }
-  });
-});
-
-app.get('/db', function(request, response) {
-  pool.query('SELECT * FROM rel_org_type', function(err, result) {
-    if (err) {
-      console.error(err);
-      response.send('Error ' + err);
-    } else {
-      response.render('pages/db', {
-        results: result.rows,
-      });
-    }
-  });
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+    app.listen(app.get('port'), function () {
+      console.log('Node app is running on port', app.get('port'));
+    });
