dcc2ef9cb60eefdd7215d7c3f98590a7dccd51af
Michael Guse <mguse@salesforce.com>
2018-08-31 01:57:11 -0500

    Made some minor main.css changes - mostly around spacing

--- sql/UPDATE-sandbox-nonpreview-214.sql
+++ sql/UPDATE-sandbox-nonpreview-214.sql
@@ -2,4 +2,4 @@ UPDATE public.rel_org_type
 SET internal_rel_name = 214,
     external_rel_name = 'Summer ''18'
 WHERE
- org_type = 'NON-PREVIEW';
+ org_type = 'Non-Preview';
--- sql/UPDATE-sandbox-nonpreview-216.sql
+++ sql/UPDATE-sandbox-nonpreview-216.sql
@@ -2,4 +2,4 @@ UPDATE public.rel_org_type
 SET internal_rel_name = 216,
     external_rel_name = 'Winter ''19'
 WHERE
- org_type = 'NON-PREVIEW';
+ org_type = 'Non-Preview';
