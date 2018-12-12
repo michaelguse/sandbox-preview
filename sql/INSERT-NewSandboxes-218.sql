03080dbad5c24b2b0329d37b6f8d34135180d6f1
Michael Guse <mguse@salesforce.com>
2018-12-12 14:25:50 -0600

    Additional SQL scripts

--- sql/INSERT-NewSandboxes-218.sql
+++ sql/INSERT-NewSandboxes-218.sql
@@ -1,9 +1,9 @@
  INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES
-    (216,'Winter ''19','CS73','NON-PREVIEW','Asia'),
-    (216,'Winter ''19','CS74','PREVIEW','Asia'),
-    (216,'Winter ''19','CS75','PREVIEW','Asia'),
-    (216,'Winter ''19','CS76','PREVIEW','Asia'),
-    (216,'Winter ''19','CS103','NON-PREVIEW','Europe'),
-    (216,'Winter ''19','CS104','PREVIEW','Europe'),
-    (216,'Winter ''19','CS137','PREVIEW','Asia'),
-    (216,'Winter ''19','CS138','PREVIEW','Americas');
+    (216,'Winter ''19','CS73','Non-Preview','Asia'),
+    (216,'Winter ''19','CS74','Preview','Asia'),
+    (216,'Winter ''19','CS75','Preview','Asia'),
+    (216,'Winter ''19','CS76','Preview','Asia'),
+    (216,'Winter ''19','CS103','Non-Preview','Europe'),
+    (216,'Winter ''19','CS104','Preview','Europe'),
+    (216,'Winter ''19','CS137','Preview','Asia'),
+    (216,'Winter ''19','CS138','Preview','Americas');
