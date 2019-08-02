4447142696bb9d52821eaf361ee1a862cf3597ce
Michael Guse <michael#optimizeconsulting.com>
2019-08-02 14:43:42 -0500

    Updates for Winter 20 preview

---
+++ sql/INSERT-NewInstances-222.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES
+    (220,'Summer ''19','CS142','Preview','Americas'),
+    (220,'Summer ''19','CS148','Non-Preview','Americas');
