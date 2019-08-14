4d6eeb292ec97610d22f05e7dac913598aab5190
Michael Guse <michael#optimizeconsulting.com>
2019-08-13 21:31:47 -0500

    Fixed the REGEX to match the split REGEX

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.get('/upgrade',
     // Form filter and validation for upgrade page 
     form(
       filter("org_id").trim().toUpper(),
-      validate("org_id").required().is(/^(\s*CS[0-9]{1,3}\s*[\s,;|]?)+$/,"We only support sandbox lookups! Please enter only valid instance numbers starting with CS!")
+      validate("org_id").required().is(/^[\s,;|]*(CS[0-9]{1,3}[\s,;|]*)+$/,"We only support sandbox lookups! Please enter only valid instance numbers starting with CS!")
    ),
     function (request, response) {
       if (!request.form.isValid) {
