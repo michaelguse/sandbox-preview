a98ac554876ced5a30ad67380b1655e70e92f66e
Michael Guse <michael#optimizeconsulting.com>
2019-12-10 10:06:06 -0600

    Added new instances to both STG and PRD

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ var pool = new Pool(config);
 var qryres = "";
 
 var express = require('express'),
-  bodyParser = require('body-parser'),
-  form = require('express-form'),
-  filter = form.filter,
-  validate = form.validate;
+    bodyParser = require('body-parser'),
+    form = require('express-form'),
+    filter = form.filter,
+    validate = form.validate;
 
 var app = express();
 
@@ -82,56 +82,55 @@ app.get('/', function (request, response) {
 
 //Sandbox upgrade page request
 app.get('/upgrade',
-    // Form filter and validation for upgrade page 
-    form(
-      filter("org_id").trim().toUpper(),
-      validate("org_id").required().is(/^[\s,;|]*(CS[0-9]{1,3}[\s,;|]*)+$/,"We only support sandbox lookups! Please enter only valid instance numbers starting with CS!")
-   ),
-    function (request, response) {
-      if (!request.form.isValid) {
-        // Handle errors
-        console.log("Instance lookup entry: ", request.form.org_id);
-        console.log(request.form.errors);
-        response.render('pages/index.ejs', { errors: request.form.errors });
-      } else {
-        var list = request.form.org_id;
-        console.log("Instance lookup entry: ", list);
-        list = list.split(/[\s,;|]+/);
-        list = list.map(Function.prototype.call, String.prototype.trim);
-        console.log("List of sandbox instances: ", list);
-        console.log("Number of sandbox instances: ", list.length);
-        pool.query('SELECT id, internal_rel_name, external_rel_name, org_id, org_type FROM rel_org_type WHERE org_id = ANY($1::text[]) ORDER BY substring(org_id, 3)::INTEGER', [list], function (err, result) {
-          if (err) {
-            console.error(err);
-            response.send('Error: ' + err);
+  // Form filter and validation for upgrade page 
+  form(
+    filter("org_id").trim().toUpper(),
+    validate("org_id").required().is(/^[\s,;|]*(CS[0-9]{1,3}[\s,;|]*)+$/,"We only support sandbox lookups! Please enter only valid instance numbers starting with CS!")
+  ),
+  function (request, response) {
+    if (!request.form.isValid) {
+      // Handle errors
+      console.log("Instance lookup entry: ", request.form.org_id);
+      console.log(request.form.errors);
+      response.render('pages/index.ejs', { errors: request.form.errors });
+    } else {
+      var list = request.form.org_id;
+      console.log("Instance lookup entry: ", list);
+      list = list.split(/[\s,;|]+/);
+      list = list.map(Function.prototype.call, String.prototype.trim);
+      console.log("List of sandbox instances: ", list);
+      console.log("Number of sandbox instances: ", list.length);
+      pool.query('SELECT id, internal_rel_name, external_rel_name, org_id, org_type FROM rel_org_type WHERE org_id = ANY($1::text[]) ORDER BY substring(org_id, 3)::INTEGER', [list], function (err, result) {
+        if (err) {
+          console.error(err);
+          response.send('Error: ' + err);
+        } else {
+          qryres = result.rows;
+          console.log("Number of results: ", qryres.length);
+          // check for empyt result set
+          if (qryres.length <= 0) {
+            console.log("[ 'Not a valid sandbox instance - try again!' ]");
+            response.render('pages/index.ejs', { errors: [ 'Not a valid sandbox instance - try again!' ], input: list });
           } else {
-            qryres = result.rows;
-            console.log("Number of results: ", qryres.length);
-            // check for empyt result set
-            if (qryres.length <= 0) {
-              console.log("[ 'Not a valid sandbox instance - try again!' ]");
-              response.render('pages/index.ejs', { errors: [ 'Not a valid sandbox instance - try again!' ], input: list });
+            if (qryres.length == 1) {
+              console.log("Single result");
+              // console.log(qryres);
+              response.render('pages/upgrade', { results: qryres });
             } else {
-              if (qryres.length == 1) {
-                console.log("Single result");
-                // console.log(qryres);
-                response.render('pages/upgrade', { results: qryres });
-              } else {
-                console.log("Multiple results");
-                response.render('pages/multi-results', { results: qryres });
-              }
+              console.log("Multiple results");
+              response.render('pages/multi-results', { results: qryres });
             }
           }
-        });
-      }
+        }
+      });
     }
+  }
 );
 
 // Cheatsheet request
 app.get('/cheatsheet', function (request, response) {
     response.render('pages/cheatsheet');
-  }
-);
+});
 
 app.get('/sandbox/types', function (request, response) {
   pool.query('SELECT count(id) AS "Count",org_type AS "Type",external_rel_name AS "Release" FROM public.rel_org_type GROUP BY org_type, external_rel_name', function (err, result) {
