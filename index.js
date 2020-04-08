e671ed1b2597c29a28a9acb4850bb4bf3d98f8ee
Michael Guse <michael#optimizeconsulting.com>
2020-04-08 10:33:59 -0500

    Replaced log info with heroku-logger

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var Pool = require('pg-pool');
 var url = require('url');
 var session = require('client-sessions');
 require('handlebars');
+var logger = require('heroku-logger');
 
 // Change the DATABASE_URL in local .env file to your own setup for local testing
 var params = url.parse(process.env.DATABASE_URL);
@@ -43,30 +44,29 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 function lookupResult(list, response) {
-  console.log("Instance lookup entry: ", list);
+  logger.info("Lookup entries submitted: ", {raw_entries: list});
   list = list.split(/[\s,;|]+/);
   list = list.map(Function.prototype.call, String.prototype.trim);
   list = list.map(Function.prototype.call, String.prototype.toUpperCase);
-  console.log("List of sandbox instances: ", list);
-  console.log("Number of sandbox instances: ", list.length);
+  logger.info("List of sandbox instances: ", {mapped_entries: list});
+  logger.info("Number of sandbox instances: ", {num_entries: list.length} );
   pool.query('SELECT id, internal_rel_name, external_rel_name, org_id, org_type FROM rel_org_type WHERE org_id = ANY($1::text[]) ORDER BY substring(org_id, 3)::INTEGER', [list], function (err, result) {
     if (err) {
-      console.error(err);
+      logger.error('Error executing query', {error: err.stack} );
       response.send('Error: ' + err);
     } else {
       qryres = result.rows;
-      console.log("Number of results: ", qryres.length);
+      logger.info("Query results", {num_results: qryres.length} );
       // check for empyt result set
       if (qryres.length <= 0) {
-        console.log("[ 'Not a valid sandbox instance - try again!' ]");
+        logger.info('Not a valid sandbox instance - try again!');
         response.render('pages/index.ejs', { errors: [ 'Not a valid sandbox instance - try again!' ], input: list });
       } else {
         if (qryres.length == 1) {
-          console.log("Single result");
-          // console.log(qryres);
+          logger.info("One query result", {result: qryres});
           response.render('pages/upgrade', { results: qryres });
         } else {
-          console.log("Multiple results");
+          logger.info("More than one query result", {result: qryres });
           response.render('pages/multi-results', { results: qryres });
         }
       }
@@ -92,14 +92,14 @@ app.use(function(req, res, next) {
     next();
   } else {
     req.session.reset();
-    console.log('Initialize session!');
+    logger.info('Query for current prod release');
     pool.query('SELECT id, internal_rel_name, external_rel_name, org_id, org_type FROM rel_org_type WHERE org_type=$1 LIMIT 1', ['Non-Preview'], function (err, result) {  
       if (err) {
-        console.error(err);
+        logger.error('Error executing query',{error: err.stack });
         res.send('Error: ' + err);
       } else {
         req.session.curr_prod_external = result.rows[0].external_rel_name;
-        console.log('New session variable - curr_prod_external: %s',req.session.curr_prod_external);
+        logger.info('Initialize new prod release variable' );
         res.locals.curr_prod_external = req.session.curr_prod_external;
         next();
       }
@@ -109,6 +109,7 @@ app.use(function(req, res, next) {
 
 // Home page request
 app.get('/', function (request, response) {
+  logger.info('Home page visit',{visit: 'home'});
   response.render('pages/index');
 });
 
@@ -120,13 +121,15 @@ app.get('/upgrade',
     validate("org_id").required().is(/^[\s,;|]*(CS[0-9]{1,3}[\s,;|]*)+$/,"We only support sandbox lookups! Please enter only valid instance numbers starting with CS!")
   ),
   function(request, response) {
+    logger.info('Web lookup page visit', {visit: 'weblookup'});
     if (!request.form.isValid) {
       // Handle errors
-      console.log("Instance lookup entry: ", request.form.org_id);
-      console.log(request.form.errors);
+      logger.info('Invalid lookup entry', {invalid_entries: request.form.org_id} );
+      logger.error('Website form errors', {form_errors: request.form.errors} );
       response.render('pages/index.ejs', { errors: request.form.errors });
     } else {
       var list = request.form.org_id;
+      logger.info('Website lookup entries',{web_entries: list});
       lookupResult(list, response);
     }
   }
@@ -134,42 +137,41 @@ app.get('/upgrade',
 
 // Cheatsheet request
 app.get('/cheatsheet', function (request, response) {
+    logger.info('Cheatsheet page visit', {visit: 'cheatsheet'});
     response.render('pages/cheatsheet');
 });
 
 app.get('/sandbox/types', function (request, response) {
+  logger.info('Sandbox Type report page visit', {visit: 'sandboxtypes'});
   pool.query('SELECT count(id) AS "Count",org_type AS "Type",external_rel_name AS "Release" FROM public.rel_org_type GROUP BY org_type, external_rel_name', function (err, result) {
     if (err) {
-      console.error(err);
+      logger.error('Error executing query',{error: err.stack });
       response.send('Error ' + err);
     } else {
-      console.log(result.rows);
-      response.render('pages/sandboxtypes', {
-        results: result.rows,
-      });
+      response.render('pages/sandboxtypes', { results: result.rows });
     }
   });
 });
 
 app.get('/sandbox/instances', function (request, response) {
+  logger.info('Sandbox Instance report page visit', {visit: 'sandboxinstances'});
   pool.query('SELECT org_id AS "Instance",org_type AS "Type", org_region AS "Region", external_rel_name AS "Release" FROM public.rel_org_type ORDER BY org_type DESC, org_region ASC, external_rel_name, substring(org_id, 3)::INTEGER', function (err, result) {
     if (err) {
-      console.error(err);
+      logger.error('Error executing query',{error: err.stack });
       response.send('Error ' + err);
     } else {
-      console.log("List of sandbox instances: ",result.rows.length);
-      response.render('pages/sandboxinstances', {
-        results: result.rows,
-      });
+      response.render('pages/sandboxinstances', {results: result.rows } );
     }
   });
 });
 
 app.get('/sandbox/:id', function (request, response) {
+  logger.info('API lookup page visit', {visit: 'apilookup'});
   var list = request.params.id;
+  logger.info('API lookup entries',{api_entries: list});
   lookupResult(list, response);
 });
 
 app.listen(app.get('port'), function () {
-  console.log('Node app is running on port', app.get('port'));
+  logger.info('Node app is running', { port: app.get('port') } );
 });
