e2cf268bc550045ccdf10b8525e49150ad3734cd
Michael Guse <michael#optimizeconsulting.com>
2020-04-08 10:44:42 -0500

    Fixed some logging messages

--- index.js
+++ index.js
@@ -44,29 +44,25 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 function lookupResult(list, response) {
-  logger.info("Lookup entries submitted: ", {raw_entries: list});
   list = list.split(/[\s,;|]+/);
   list = list.map(Function.prototype.call, String.prototype.trim);
   list = list.map(Function.prototype.call, String.prototype.toUpperCase);
-  logger.info("List of sandbox instances: ", {mapped_entries: list});
-  logger.info("Number of sandbox instances: ", {num_entries: list.length} );
+  logger.info("Number of sandbox params: ", {num_entries: list.length} );
   pool.query('SELECT id, internal_rel_name, external_rel_name, org_id, org_type FROM rel_org_type WHERE org_id = ANY($1::text[]) ORDER BY substring(org_id, 3)::INTEGER', [list], function (err, result) {
     if (err) {
       logger.error('Error executing query', {error: err.stack} );
       response.send('Error: ' + err);
     } else {
       qryres = result.rows;
-      logger.info("Query results", {num_results: qryres.length} );
+      logger.info("Query result size", {num_results: qryres.length} );
       // check for empyt result set
       if (qryres.length <= 0) {
         logger.info('Not a valid sandbox instance - try again!');
         response.render('pages/index.ejs', { errors: [ 'Not a valid sandbox instance - try again!' ], input: list });
       } else {
         if (qryres.length == 1) {
-          logger.info("One query result", {result: qryres});
           response.render('pages/upgrade', { results: qryres });
         } else {
-          logger.info("More than one query result", {result: qryres });
           response.render('pages/multi-results', { results: qryres });
         }
       }
