c97f9884e8006d4ee975f292bd4d80141070dccc
Michael Guse <michael#optimizeconsulting.com>
2020-08-17 20:26:24 -0500

    Added script to add CS160 and CS162 to the DB

---
+++ sql/INSERT-NewInstances-226_20200817.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES
+    (226,'Summer ''20','CS162','Non-Preview','Europe'),
+    (226,'Summer ''20','CS160','Preview','Europe');
