f648d87d564352a32c15317cf4173206867730ef
Michael Guse <michael#optimizeconsulting.com>
2021-02-12 20:23:51 -0600

    Added sandboxes instances for 230

---
+++ sql/INSERT-NewInstances-230_20210211.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES
+    (230,'Spring ''21','AUS2S','Preview','Asia'),
+    (230,'Winter ''21','AUS4S','Non-Preview','Asia'),
+    (230,'Spring ''21','USA4S','Preview','Americas'),
+    (230,'Spring ''21','USA6S','Preview','Americas');
