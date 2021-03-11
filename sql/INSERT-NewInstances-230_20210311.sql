8009240a98a8a5de48caf6c56f53613445cab14e
Michael Guse <michael#optimizeconsulting.com>
2021-03-11 10:51:38 -0600

    Added 4 new sandboxes to the DB

---
+++ sql/INSERT-NewInstances-230_20210311.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES
+    (230,'Spring ''21','CS166','Preview','Americas'),
+    (230,'Spring ''21','CS171','Non-Preview','Americas'),
+    (230,'Spring ''21','CS172','Preview','Americas'),
+    (230,'Spring ''21','CS167','Preview','Americas');
