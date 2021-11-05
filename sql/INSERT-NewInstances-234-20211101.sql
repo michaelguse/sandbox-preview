0566fc871c0a96e0ab2b0ab85d12a08432c01c21
Michael Guse <michael#optimizeconsulting.com>
2021-11-05 15:08:24 -0500

    Added SQL script for new sandbox instances

---
+++ sql/INSERT-NewInstances-234-20211101.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES
+    (234,'Winter ''22','AUS14S','Preview','Asia'),
+    (234,'Winter ''22','AUS28S','Non-Preview','Asia'),
+    (234,'Winter ''22','AUS36S','Preview','Asia'),
+    (234,'Winter ''22','USA20S','Preview','Americas'),
+    (234,'Winter ''22','USA22S','Non-Preview','Americas');
