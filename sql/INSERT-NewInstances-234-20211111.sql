c8c5aa6a27be1a122afd12e4ed3975484caeb4a4
Michael Guse <michael#optimizeconsulting.com>
2021-11-15 09:59:01 -0600

    Added 5 new sandbox instances

---
+++ sql/INSERT-NewInstances-234-20211111.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES
+    (234,'Winter ''22','IND14S','Non-Preview','Asia'),
+    (234,'Winter ''22','IND20S','Preview','Asia'),
+    (234,'Winter ''22','IND10S','Preview','Asia'),
+    (234,'Winter ''22','IND22S','Preview','Asia'),
+    (234,'Winter ''22','IND8S','Preview','Asia');
