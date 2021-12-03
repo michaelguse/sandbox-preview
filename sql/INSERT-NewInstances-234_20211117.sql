d2a230968153499a76caa49dc9b7c8933ab4eb92
Michael Guse <michael#optimizeconsulting.com>
2021-12-03 09:57:04 -0600

    Added new instances

---
+++ sql/INSERT-NewInstances-234_20211117.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES
+    (234,'Winter ''22','DEU6S','Preview','Europe'),
+    (234,'Winter ''22','DEU8S','Non-Preview','Europe');
---
+++ sql/INSERT-NewInstances-234_20211201.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES
+    (234,'Winter ''22','JPN20S','Preview','Asia'),
+    (234,'Winter ''22','JPN22S','Preview','Asia'),
+    (234,'Winter ''22','JPN24S','Non-Preview','Asia'),
+    (234,'Winter ''22','JPN28S','Preview','Asia'), 
+    (234,'Winter ''22','AUS16S','Non-Preview','Asia'),
+    (234,'Winter ''22','AUS18S','Preview','Asia'),
+    (234,'Winter ''22','AUS20S','Preview','Asia'),
+    (234,'Winter ''22','AUS38S','Preview','Asia');
