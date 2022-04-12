7c77b307dbf0ec767a50bb42592d463b50484147
Michael Guse <michael#optimizeconsulting.com>
2022-04-12 11:51:20 -0500

    Added 4 new sandbox instances

---
+++ sql/INSERT-NewInstances-236-20220412.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES
+    (236,'Spring ''22','CS308','Preview','Americas'),
+    (236,'Spring ''22','GBR2S','Preview','Europe'),
+    (236,'Spring ''22','GBR4S','Non-Preview','Europe'),
+    (236,'Spring ''22','CAN20S','Non-Preview','Americas');
