3c684f716f07e0fd3ba0d1e28b2c9ee9a5c2e41c
Michael Guse <michael#optimizeconsulting.com>
2022-08-18 07:37:20 -0500

    Added 6 new instances

---
+++ sql/INSERT-NewInstance-238_20220818.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES
+    (238,'Summer ''22','KOR2S','Preview','Asia'),
+    (238,'Summer ''22','KOR4S','Non-Preview','Asia'),
+    (238,'Summer ''22','SWE2S','Preview','Europe'),
+    (238,'Summer ''22','SWE4S','Non-Preview','Europe'),
+    (238,'Summer ''22','USA9906S','Preview','Americas'),
+    (238,'Summer ''22','USA9916S','Non-Preview','Americas');
