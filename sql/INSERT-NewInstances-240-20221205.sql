f795312c148b653f4f8f70c4acb263f4b5a6c662
Michael Guse <michael#optimizeconsulting.com>
2022-12-06 08:48:49 -0600

    Added new sandboxes to SPG

---
+++ sql/INSERT-NewInstances-240-20221205.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES
+    (240,'Winter ''23','IND54S','Non-Preview','Asia'),
+    (240,'Winter ''23','CS263','Preview','Americas'),
+    (240,'Winter ''23','CS264','Non-Preview','Americas'),
+    (240,'Winter ''23','CS364','Preview','Europe'),
+    (240,'Winter ''23','CS365','Preview','Europe');
