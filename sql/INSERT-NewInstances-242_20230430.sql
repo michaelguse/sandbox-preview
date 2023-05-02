cba2cb615b29c61cfdae53e358aa33a5d65b308e
Michael Guse <guse.michael@gmail.com>
2023-05-02 11:34:04 -0500

    Added USA224S sandbox

---
+++ sql/INSERT-NewInstances-242_20230430.sql
@@ -0,0 +1,10 @@
+BEGIN ;
+
+/*
+USA224S	Preview	SB1	Americas	4/29/2023		Added - new sandbox
+*/
+
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES
+    (242,'Spring ''23','USA224S','Preview','Americas');
+
+COMMIT ;
