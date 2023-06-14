22aeaba57fd5476df4b79948749628c828948a43
Michael Guse <guse.michael@gmail.com>
2023-06-14 11:15:12 -0500

    Added new instance AUS80S

---
+++ sql/INSERT-NewInstances-244_20230614.sql
@@ -0,0 +1,10 @@
+BEGIN ;
+
+/*
+AUS80S	Non-Preview	R2b	Asia	6/14/2023		Added - new sandbox
+*/
+
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES  
+    (244,'Summer ''23','AUS80S','Non-Preview','Asia');	   
+
+COMMIT ;
