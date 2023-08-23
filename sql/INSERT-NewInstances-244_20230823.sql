e9876d634d3ba5906834dcdc351a8a247391382b
Michael Guse <guse.michael@gmail.com>
2023-08-23 14:28:04 -0500

    Added new sandbox DEU62S

---
+++ sql/INSERT-NewInstances-244_20230823.sql
@@ -0,0 +1,10 @@
+BEGIN ;
+
+/*
+DEU62S  Non-Preview R2a Europe  8/23/2023   Added - new sandbox
+*/
+
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES  
+    (244,'Summer ''23','DEU62S','Non-Preview','Europe');
+
+COMMIT ;
