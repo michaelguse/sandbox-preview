cf727bc51d8775dd599d8c1255ea8c05bbc9a6b7
Michael Guse <guse.michael@gmail.com>
2023-12-19 13:08:21 -0600

    Added two new instances

---
+++ sql/INSERT-NewInstances-246_20231219.sql
@@ -0,0 +1,12 @@
+BEGIN ;
+
+/* 
+USA792S	Non-Preview	R2a	Americas	12/19/2023		Added - New sandbox
+USA804S	Non-Preview	R2a	Americas	12/19/2023		Added - New sandbox
+*/
+
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES  
+    (246,'Winter ''24','USA792S','Non-Preview','Americas'),
+    (246,'Winter ''24','USA804S','Non-Preview','Americas');
+
+COMMIT ;
