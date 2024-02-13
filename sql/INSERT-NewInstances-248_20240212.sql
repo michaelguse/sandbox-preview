84e152f586026d4721cbecc878bc9db7a19891f9
Michael Guse <guse.michael@gmail.com>
2024-02-13 16:28:59 -0600

    Post go live Spring '24 updates

---
+++ sql/INSERT-NewInstances-248_20240212.sql
@@ -0,0 +1,20 @@
+BEGIN ;
+
+/* 
+usa892s	Non-Preview	R2a	Americas	2/12/2024		Added - New sandbox
+usa894s	Non-Preview	R2a	Americas	2/12/2024		Added - New sandbox
+usa896s	Non-Preview	R2a	Americas	2/12/2024		Added - New sandbox
+usa898s	Non-Preview	R2a	Americas	2/12/2024		Added - New sandbox
+usa900s	Non-Preview	R2a	Americas	2/12/2024		Added - New sandbox
+usa902s	Non-Preview	R2a	Americas	2/12/2024		Added - New sandbox
+*/
+
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES  
+    (248,'Spring ''24','USA892S','Non-Preview','Americas'),
+    (248,'Spring ''24','USA894S','Non-Preview','Americas'),
+    (248,'Spring ''24','USA896S','Non-Preview','Americas'),
+    (248,'Spring ''24','USA898S','Non-Preview','Americas'),
+    (248,'Spring ''24','USA900S','Non-Preview','Americas'),
+    (248,'Spring ''24','USA902S','Non-Preview','Americas');
+
+COMMIT ;
