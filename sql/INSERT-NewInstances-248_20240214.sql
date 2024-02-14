380641b6ee7682a937b578409c55ba702ed480a3
Michael Guse <guse.michael@gmail.com>
2024-02-14 11:54:04 -0600

    Added six new sandboxes

---
+++ sql/INSERT-NewInstances-248_20240214.sql
@@ -0,0 +1,20 @@
+BEGIN ;
+
+/* 
+usa880s	Non-Preview	R2a	Americas	2/14/2024		Added - New sandbox
+usa882s	Non-Preview	R2a	Americas	2/14/2024		Added - New sandbox
+usa884s	Non-Preview	R2a	Americas	2/14/2024		Added - New sandbox
+usa886s	Non-Preview	R2a	Americas	2/14/2024		Added - New sandbox
+usa888s	Non-Preview	R2a	Americas	2/14/2024		Added - New sandbox
+usa890s	Non-Preview	R2a	Americas	2/14/2024		Added - New sandbox
+*/
+
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES  
+    (248,'Spring ''24','USA880S','Non-Preview','Americas'),
+    (248,'Spring ''24','USA882S','Non-Preview','Americas'),
+    (248,'Spring ''24','USA884S','Non-Preview','Americas'),
+    (248,'Spring ''24','USA886S','Non-Preview','Americas'),
+    (248,'Spring ''24','USA888S','Non-Preview','Americas'),
+    (248,'Spring ''24','USA890S','Non-Preview','Americas');
+
+COMMIT ;
