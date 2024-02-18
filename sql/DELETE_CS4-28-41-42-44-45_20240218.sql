579038c86ca26ee35b70fe1c23eee24debc91613
Michael Guse <guse.michael@gmail.com>
2024-02-18 15:41:28 -0600

    Deleted 6 sandboxes

---
+++ sql/DELETE_CS4-28-41-42-44-45_20240218.sql
@@ -0,0 +1,11 @@
+/*
+CS28	Preview			4/22/2020	2/18/2024	Removed
+CS4	    Preview			4/22/2020	2/18/2024	Removed
+CS41	Preview			4/22/2020	2/18/2024	Removed
+CS42	Preview			4/22/2020	2/18/2024	Removed
+CS44	Preview			4/22/2020	2/18/2024	Removed
+CS45	Preview			4/22/2020	2/18/2024	Removed
+*/
+
+DELETE FROM public.rel_org_type
+WHERE org_id in ('CS4','CS28','CS41','CS42','CS44','CS45');
