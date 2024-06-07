52d3eb4c44cb01a53517ae368398e7bce6977021
Michael Guse <guse.michael@gmail.com>
2024-06-07 12:53:40 -0500

    Added CS330 sandbox instance

---
+++ sql/INSERT-NewInstance-248_20240601.sql
@@ -0,0 +1,10 @@
+BEGIN ;
+
+/*
+CS330	Non-Preview	    R2a	    Europe	    6/1/2024
+*/
+
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES  
+    (248,'Spring ''24','CS330','Non-Preview','Europe');
+
+COMMIT ;
