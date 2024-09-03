f81706d2cf5e2af888b87ba393c72312ee50e918
Michael Guse <guse.michael@gmail.com>
2024-09-03 09:22:01 -0500

    Added one new sandbox instance

---
+++ sql/INSERT-NewInstances-250_20240903.sql
@@ -0,0 +1,10 @@
+BEGIN ;
+
+/*
+CS329	Non-Preview	R2a	Europe	9/3/2024		Added - New sandbox
+*/
+
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES  
+    (250,'Summer ''24','CS329','Non-Preview','Europe');
+
+COMMIT ;
