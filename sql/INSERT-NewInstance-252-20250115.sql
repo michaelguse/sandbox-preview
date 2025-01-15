bc6baf6fc5b09b2ab7a496579da941005c9c2a59
Michael Guse <guse.michael@gmail.com>
2025-01-15 14:00:03 -0600

    Added new sandbox and fixed an org_id issue

---
+++ sql/INSERT-NewInstance-252-20250115.sql
@@ -0,0 +1,10 @@
+BEGIN ;
+
+/*
+USA9918S	Non-Preview		Americas	1/15/2025		Added - New sandbox
+*/
+
+INSERT INTO public.rel_org_type (internal_rel_name,external_rel_name,org_id,org_type,org_region) VALUES  
+    (252,'Winter ''25','USA9918S','Non-Preview','Americas');
+
+COMMIT ;
---
+++ sql/UPDATE-sandbox-USA9918S_20250115.sql
@@ -0,0 +1,5 @@
+UPDATE public.rel_org_type
+SET org_id = 'USA9918S'
+WHERE
+ id = 473 AND
+ org_id = 'CS329';
